91a96af66758249cf93038558cf7ef7670fd2a0c
JDogg1329 <JDogg1329@gmail.com>
2016-05-14 22:52:30 +1000

    Created checkpoint screen
    Checkpoint screen layout created

--- Android/JupiterTrail/app/src/main/java/com/example/samuel/jupitertrail/GameScreens/CheckpointScreen.java
+++ Android/JupiterTrail/app/src/main/java/com/example/samuel/jupitertrail/GameScreens/CheckpointScreen.java
@@ -3,8 +3,14 @@ package com.example.samuel.jupitertrail.GameScreens;
 import android.app.Activity;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.samuel.jupitertrail.Enum.ResourceEnum;
 import com.example.samuel.jupitertrail.R;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -18,5 +24,23 @@ public class CheckpointScreen extends ContextWrapper {
         Activity a = (Activity) context;
 
         a.setContentView(R.layout.checkpoint_screen);
+
+        final Button fuelButton = (Button) a.findViewById(R.id.btn_Doctor);
+        fuelButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Game.Ship.AddResource(ResourceEnum.Fuel);
+               // fuelText.setText(String.valueOf(Game.Ship.ResourceList.get(ResourceEnum.Fuel).Amount));
+               // UpdateStorageText();
+
+                OpenDoctorScreen();
+            }
+        });
+    }
+    private void OpenDoctorScreen()
+    {
+        // Dummy function
+        final TextView levelText =  (TextView) a.findViewById(R.id.levelTxt);
+
+        levelText.setText("Level: " + "1");
     }
 }
