ebab90b9aca4a52c2ed3a4115c5838ff91eecb73
samsmyname <9506047@student.swin.edu.au>
2016-05-23 00:34:59 +1000

    added unit test class

--- Android/JupiterTrail/app/src/test/java/com/example/samuel/jupitertrail/CrewMemberUnitTests.java
+++ Android/JupiterTrail/app/src/test/java/com/example/samuel/jupitertrail/CrewMemberUnitTests.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class ExampleUnitTest {
+public class CrewMemberUnitTests {
     @Test
     public void crew_resets () throws Exception {
         CrewMember testCrewman = new CrewMember();
@@ -31,5 +31,37 @@ public class ExampleUnitTest {
         assertEquals(testCrewman.PermImmune, true);
         assertEquals(testCrewman.Alive, false);
     }
-    
+    @Test
+    public void crew_status_deceased() throws Exception{
+        CrewMember testCrewman = new CrewMember();
+
+        testCrewman.Alive = false;
+        String expected = "Deceased";
+
+        assertEquals(testCrewman.Status(), expected);
+
+
+    }
+    @Test
+    public void crew_status_healthy() throws Exception{
+        CrewMember testCrewman = new CrewMember();
+
+        testCrewman.Alive = true;
+        String expected = "Healthy";
+
+        assertEquals(testCrewman.Status(), expected);
+
+
+    }
+    @Test
+    public void crew_status_ill() throws Exception{
+        CrewMember testCrewman = new CrewMember();
+
+        testCrewman.Alive = true;
+        testCrewman.KyraxianPetris = true;
+        String expected = "Ill";
+
+        assertEquals(testCrewman.Status(), expected);
+
+    }
 }
