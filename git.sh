0a42a6c9110fcf1d6e62669151e17b9f82ec406c
samsmyname <9506047@student.swin.edu.au>
2016-05-24 02:32:15 +1000

    Signed-off-by: samsmyname <9506047@student.swin.edu.au>

--- git.sh
+++ git.sh
@@ -1,5 +1,5 @@
 git config --global user.email "9506047@student.swin.edu.au"
 git config --global user.name "samsmyname"
 git pull
-git add Android\JupiterTrail\app\build\outputs\apk
-git commit -m "a jenkins autobuild" Android\JupiterTrail\app\build\outputs\apk
+git add .
+git commit -am "a jenkins autobuild"
