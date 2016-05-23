36e0a2e8ac87035fabc911cf0ad20af5e06198e2
samsmyname <9506047@student.swin.edu.au>
2016-05-24 01:48:18 +1000

    Signed-off-by: samsmyname <9506047@student.swin.edu.au>

--- git.sh
+++ git.sh
@@ -2,4 +2,4 @@ git config --global user.email "9506047@student.swin.edu.au"
 git config --global user.name "samsmyname"
 git pull
 git add .
-git commit -am "jenkins autobuild"
+git commit -am "a jenkins autobuild"
